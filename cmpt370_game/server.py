83e613c0a157a5b4368360a6b7363401a6ef5343
alesa30garcia <garciaa37@mymacewan.ca>
2025-11-29 13:49:12 -0700

    Add files via upload

---
+++ cmpt370_game/server.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+import webbrowser
+from sys import exit
+from livereload import Server
+PORT = 8000
+
+try:
+    webbrowser.open_new_tab(f'http://localhost:{PORT}')
+    server = Server()
+    server.watch('*.html')
+    server.watch('js/*.js')
+    server.serve(root='.', port=PORT) 
+
+except KeyboardInterrupt:
+    exit()
