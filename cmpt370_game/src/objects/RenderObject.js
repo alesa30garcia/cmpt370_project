e03c569eb7956b1df199f96de69f84f1aed4572c
alesa30garcia <garciaa37@mymacewan.ca>
2025-12-01 11:29:40 -0700

    Enhance RenderObject with texture coordinates
    Updated the RenderObject class to include texture coordinates and improved logging for vertex and normal counts.

--- cmpt370_game/src/objects/RenderObject.js
+++ cmpt370_game/src/objects/RenderObject.js
@@ -1,4 +1,3 @@
-
 class RenderObject {
   constructor(glContext, object) {
     this.gl = glContext;
@@ -68,20 +67,21 @@ class RenderObject {
     const normals = new Float32Array(this.model.normals);
     const indices = new Uint16Array(this.model.triangles);
 
-    //const textureCoords = new Float32Array(this.model.uvs);
+    let textureCoords = new Float32Array(this.model.uvs);
     var vertexArrayObject = this.gl.createVertexArray();
     this.gl.bindVertexArray(vertexArrayObject);
-
+    console.log("vertices ", positions.length,"normals ", normals.length, "uvs", textureCoords.length);
     this.buffers = {
       vao: vertexArrayObject,
       attributes: {
         position: initPositionAttribute(this.gl, this.programInfo, positions),
         normal: initNormalAttribute(this.gl, this.programInfo, normals),
-        // uv: initTextureCoords(this.gl, this.programInfo, textureCoords),
+        uv: initTextureCoords(this.gl, this.programInfo, textureCoords),
       },
       indicies: initIndexBuffer(this.gl, indices),
       numVertices: indices.length
     }
+
   }
 
   lightingShader() {
@@ -95,7 +95,7 @@ class RenderObject {
       attribLocations: {
         vertexPosition: this.gl.getAttribLocation(shaderProgram, 'aPosition'),
         vertexNormal: this.gl.getAttribLocation(shaderProgram, 'aNormal'),
-        // vertexUV: this.gl.getAttribLocation(shaderProgram, 'aUV'),
+        vertexUV: this.gl.getAttribLocation(shaderProgram, 'aUV'),
       },
       uniformLocations: {
         projection: this.gl.getUniformLocation(shaderProgram, 'uProjectionMatrix'),
@@ -103,16 +103,16 @@ class RenderObject {
         model: this.gl.getUniformLocation(shaderProgram, 'uModelMatrix'),
         // normalMatrix: this.gl.getUniformLocation(shaderProgram, 'normalMatrix'),
         diffuseVal: this.gl.getUniformLocation(shaderProgram, 'diffuseVal'),
-        // ambientVal: this.gl.getUniformLocation(shaderProgram, 'ambientVal'),
-        // specularVal: this.gl.getUniformLocation(shaderProgram, 'specularVal'),
+        //ambientVal: this.gl.getUniformLocation(shaderProgram, 'ambientVal'),
+        //specularVal: this.gl.getUniformLocation(shaderProgram, 'specularVal'),
         // nVal: this.gl.getUniformLocation(shaderProgram, 'nVal'),
         // cameraPosition: this.gl.getUniformLocation(shaderProgram, 'uCameraPosition'),
         // numLights: this.gl.getUniformLocation(shaderProgram, 'numLights'),
         // lightPositions: this.gl.getUniformLocation(shaderProgram, 'uLightPositions'),
         // lightColours: this.gl.getUniformLocation(shaderProgram, 'uLightColours'),
         // lightStrengths: this.gl.getUniformLocation(shaderProgram, 'uLightStrengths'),
-        // sampler: this.gl.getUniformLocation(shaderProgram, 'uTexture'),
-        // samplerExists: this.gl.getUniformLocation(shaderProgram, "samplerExists")
+        sampler: this.gl.getUniformLocation(shaderProgram, 'uTexture'),
+        samplerExists: this.gl.getUniformLocation(shaderProgram, "samplerExists")
       },
     };
     shaderValuesErrorCheck(programInfo);
